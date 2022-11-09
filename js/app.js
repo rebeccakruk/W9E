a292d56bd7d49a14d1588e7cd7bf540ae38a055a
rk <rebeccakruk@outlook.com>
2022-11-08 19:30:35 -0700

    content

---
+++ js/app.js
@@ -0,0 +1,60 @@
+let twits = [
+                {tweet: `I kid you not a woman is watching Shrek next me on the tube loudly from her phone and I looked over at it and she turned her phone so I could also watch Shrek`,
+                userName: `@mmtowns`,
+                createdAt: `03-29-2022`,
+                age: 29
+            },
+            {
+                tweet: `For the past 20 years I've received a Valentines card from the same secret admirer. So I was pretty upset when I didn't get one this year. First my granny dies and now this.`,
+                userName: `@colmflynnire`,
+                createdAt: `02-14-2022`,
+                age: 37
+            },
+            {
+                tweet: `my love language is cooking elaborate meals screaming at everyone to get out of the kitchen then loudly announcing the food was NOT MY BEST and waiting for compliments`,
+                userName: `@LilyYily`,
+                createdAt: `03-20-2022`,
+                age: 38
+            },
+            {
+                tweet: `Well well well, if it isn't the consequences of my own actions`,
+                userName: `Megan Townsend`,
+                createdAt: `03-29-2022`,
+                age: 44
+            },
+            {
+                tweet: `Crossfit is just Fight Club if the first two rules were reversed`,
+                userName: `@kibblesmith`,
+                createdAt: `03-09-2015`,
+                age: 30
+            },
+            {
+                tweet: `Anybody here named Jeff? (Jeff: yes) (Geoff: yeos)`,
+                userName: `@mtobey`,
+                createdAt: `01-20-2016`,
+                age: 33
+            },
+            {
+                tweet: `Hi Grandma. Can you please come pick me up from my rap battle? It's over. I lost. He saw you drop me off and did a pretty devastating rhyme about it`,
+                userName: `@Ch000ch`,
+                createdAt: `10-19-2013`,
+                age: 17
+            },
+            {
+                tweet: `Relationships are mostly you apologizing for saying something hilarious`,
+                userName: `@briangaar`,
+                createdAt: `11-14-2012`,
+                age: 15
+            },
+            {
+                tweet: `You don't see a lot of brown cars anymore. When I was a kid it was just doodoo brown Oldsmobiles all over`,
+                userName: `@drewmagary`,
+                createdAt: `01-26-2022`,
+                age: 40
+            },
+            {
+                tweet: `Guy on Jeopardy was just introduced as 'Stay-at-home Uncle'`,
+                userName: `@Lizzym03`,
+                createdAt: `02-07-2022`,
+                age: 17
+            }]
